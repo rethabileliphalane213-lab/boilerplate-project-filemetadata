c546d71e8533ffa08613b8a11927d78e55854b05
Rethabile <rethabileliphalane213@gmail.com>
2026-06-03 10:03:53 +0000

    fix file name

--- index.js
+++ index.js
@@ -13,14 +13,18 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post("/api/fileanalyse",upload.single("upfile"),(req,res)=>{
-res.json({
-  name:req.file.originalname,
-  type:req.file.mimetype,
-  size:req.file.size
-})
-})
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
 
+  if (!req.file) {
+    return res.json({ error: "No file uploaded" })
+  }
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
