6efa0a34943cd552276e9d20f826338bfc94cf55
Rethabile <rethabileliphalane213@gmail.com>
2026-06-05 10:44:42 +0000

    fix multer issues

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+'use strict';
+
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+
+var multer = require('multer');
+// here on HyperDev the fs is read only, 
+// You have to upload the file to memory
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
 var app = express();
 
@@ -10,19 +15,25 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
-  console.log(req.file);
-
-  res.json({
-    name: req.file.originalname,
-    type: req.file.mimetype,
-    size: req.file.size
+     res.sendFile(process.cwd() + '/views/index.html');
   });
+    
+  // using 'multer' middleware...
+app.post('/api/fileanalyse',upload.single('upfile'), function(req, res){
+   res.json({
+    'name' : req.file.originalname,
+    'type' : req.file.mimetype,
+    'size' : req.file.size
+   });
 });
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    
+
+ // 404-NOT FOUND Middleware
+ app.use(function(req, res, next){
+   res.status(404);
+   res.type('txt').send('Not found');
+ });
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
 });
