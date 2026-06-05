9934e6447e533c8754897af15e4f9c65caec394a
Rethabile <rethabileliphalane213@gmail.com>
2026-06-05 10:49:35 +0000

    fix multer issues

--- index.js
+++ index.js
@@ -3,11 +3,9 @@
 var express = require('express');
 var cors = require('cors');
 
-var multer = require('multer');
-// here on HyperDev the fs is read only, 
-// You have to upload the file to memory
-var storage = multer.memoryStorage();
-var upload = multer({ storage: storage });
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 var app = express();
 
@@ -15,24 +13,21 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-    
-  // using 'multer' middleware...
-app.post('/api/fileanalyse',upload.single('upfile'), function(req, res){
-   res.json({
-    'name' : req.file.originalname,
-    'type' : req.file.mimetype,
-    'size' : req.file.size
-   });
+  res.sendFile(process.cwd() + '/views/index.html');
 });
-    
 
- // 404-NOT FOUND Middleware
- app.use(function(req, res, next){
-   res.status(404);
-   res.type('txt').send('Not found');
- });
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
+app.use(function(req, res) {
+  res.status(404);
+  res.type('txt').send('Not found');
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
